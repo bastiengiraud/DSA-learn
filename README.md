fe8adbb1e423b62baef3f8ba87d4443c508278f5
Bastien Giraud <bagir@dtu.dk>
2025-03-06 12:04:31 +0100

    update init.jl

--- README.md
+++ README.md
@@ -15,44 +15,27 @@ For more information, read our paper 'A Dataset Generation Toolbox for Dynamic S
 
 ## Setting up the Environment
 
-To get started with this repository, follow these steps:
+To get started with this repository, you simply need to clone the repository. All the dependencies are located in the Manifest.toml and project.toml files. All the dependencies will be installed on your machine when you run the main.jl file. For an additional overview of the packages used and their versions, you can go to functions/env.jl. But now, you can get started by:
 
 1. **Clone the Repository**  
    Clone the repository to your local machine:
 
    ```bash
    git clone https://github.com/bastiengiraud/DSA-learn.git
-   cd yourrepository
    ```
 
-2. **Acivate the Julia Environment**  
-   In the Julia REPL, activate the environment (this is included in the main file!):
-
-  ```bash
-  using Pkg
-  Pkg.activate(".")
-  ```
-
-3. **Install Dependencies**  
-  Install the required dependencies (this is also included in the main file!):
-
-  ```bash
-  using Pkg
-  Pkg.instantiate()
-  ```
-
-4. **Install R**
+2. **Install R**
   This repository also uses R and calls it using Rcall in Julia. Therefore, make sure you have R installed on your device.
   Moreover, you will need to install the following two packages in R: Rcpp, and volesti. You can install them as follows:
 
-  - Open R terminal and type: 
+  - Open a terminal in R and simply type: 
     - install.packages("Rcpp")
     - install.packages("volesti")
 
 
 ## How to Use
 
-To start using the toolbox, you simply need to specify your dataset parameters in the `init.jl` file, and then you can run the `main.jl` file. 
+To start using the toolbox, you simply need to specify your dataset parameters in the `init.jl` file, and then you can run the `main.jl` file. The `init.jl` file contains ALL parameters you can modify. Behind the parameters, a short explanation is provided of what it is and what you can choose from.
 
 1. **Configure Parameters**:  
    All parameters required for dataset generation are specified in `init.jl`. This includes configurations for both the proposed method and the benchmark methods.
--- init.jl
+++ init.jl
@@ -17,13 +17,13 @@ lhc_macros, lhc_samples, opt_dataset_filename, opt_samples, imp_dataset_filename
 stability_ub, temp_folder
 
 # include path for power systems steady state data
-case_number =               "162"
-tag =			            "SD6"
-data_path =                 joinpath(@__DIR__, "cases/static/") 
-file_path =                 joinpath(data_path, "pglib_opf_case39_epri.m") # pglib_opf_case39_epri.m pglib_opf_case162_ieee_dtc.m
+case_number =               "162"                                               # this is the number of buses
+tag =			            "SD6"                                               # this is simply a unique tag for the dataset file names
+data_path =                 joinpath(@__DIR__, "cases/static/")                 # create a file path for the network data
+file_path =                 joinpath(data_path, "pglib_opf_case39_epri.m")      # pglib_opf_case39_epri.m pglib_opf_case162_ieee_dtc.m
 
 # file directory for dynamic data
-dir_dynamics =              joinpath(@__DIR__, "cases/dynamic/") 
+dir_dynamics =              joinpath(@__DIR__, "cases/dynamic/")                # create a file path for the machine parameter data
 case_name =                 "case$(case_number)" #
 
 # file directory for storing datasets
@@ -53,37 +53,37 @@ nb_imp_samples =            10000
 
 # initialize data
 network_data = PowerModels.parse_file(file_path) 
-push_load_pf(network_data) # add the powerfactor of the loads to the dataset
+push_load_pf(network_data)                                                          # add the powerfactor of the loads to the dataset
 
-# specify sampling methods
+# specify sampling methods. Indicate if you want to do small-signal stability analysis, directed walks and/or importance sampling (mvnd_sampling)
 sss_analysis =              true
 directed_walks =            true
 mvnd_sampling =             false
-variable_loads =            keys(Dict{Int64, Any}())  # 4 => nothing add the INDICES of the loadvars'
+variable_loads =            keys(Dict{Int64, Any}())                                # if you want to add uncertain loads, specify them like this (4 => nothing) 
 
 # specify number of hyperplanes and samples in polytope and mvnd 
-hyperplanes =               400
-polytope_samples =          4000
+hyperplanes =               400                                                     # this specifies the number of hyperplanes constructed
+polytope_samples =          4000                                                    # this specifies the number of initialization points for the directed walks
 mvnd_samples =              1
 
 # specify stopping criteria for hyperplane generation
-stopping_iteration =        100
-stopping_percentage =       0.05
+stopping_iteration =        100                                                     # after this many iterations of no volume reduction, the DW algorithm stops
+stopping_percentage =       0.05                                                    # this is the volume percentage the polytope needs to be reduced by for the stopping criterion
 
 # specify infeasible contingencies, determine which line outages you want to consider for N-1 security
-contingencies_n1 =          []
-contingencies_inf =         [] 
+contingencies_n1 =          []                                                      # if you want to add line contingencies, specifiy the index of the lines
+contingencies_inf =         []                                                      # if you know which lines will result in an infeasible opf, you can exclude them here
 
 # define stability boundary 
-stability_bound =           0.03
+stability_bound =           0.03 # this is the minimum damping ratio
 #stability_margin =          0.002
-stability_lb =              0.0275
-stability_ub =              0.0325
+stability_lb =              0.0275                                                  # this is the lower bound of the HIC region
+stability_ub =              0.0325                                                  # and this is the upper bound
 
 # specify for how many of the eigenvalues closest to the imaginary axis you want to do DWs
-dw_computation =            "parallel"
-k_max =                     40
-k_max_HIC =                 15
+dw_computation =            "parallel"                                              # if you can do parallel computing of the DWs, choose: "parallel", otherwise: "series"
+k_max =                     40                                                      # maximum number of directed walk steps for each initialization point
+k_max_HIC =                 15                                                      # maximum number of directed walk steps when the HIC region is reached
 distance =                  [0.015, 0.01, 0.005]
 alpha =                     [4, 3, 2, 1] # [2, 1.5, 1, 0.5]
 
@@ -94,7 +94,7 @@ temp_folder = joinpath(parent_temp, custom_temp) # create path for folder
 
 # set load profile
 nominal_load = true
-scale_all_demand(network_data, 0.8) # scale nominal load profile with constant
+scale_all_demand(network_data, 0.8)                                                 # scale nominal load profile with constant
 
 if nominal_load != true
     nb_load = length(network_data["load"])
